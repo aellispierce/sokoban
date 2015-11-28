78a8fec241825efcf66a44668a6cbfdb4ab5e8fd
Ashley Ellis <ashley@spreedly.com>
2015-11-28 14:03:47 -0500

    Board creates a new person and returns their location

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,18 @@
 require "spec_helper"
 
 describe Board do
+  describe "#initialize" do
+    it "takes a level parameter" do
+      expect { Board.new }.to raise_error(ArgumentError)
+    end
+
+    it "creates a new person" do
+      board = Board.new(level: 1)
+
+      expect(board.person).to be_kind_of(Person)
+    end
+  end
+
   describe "#to_s" do
     it "displays the board as a string" do
       board = Board.new(level: 4)
@@ -21,6 +33,14 @@ describe Board do
         #    #    ##
         ###########
       EOS
+    end
+  end
+
+  describe "#person_location" do
+    it "returns the coordinate location of the person" do
+      board = Board.new(level: 1)
+
+      expect(board.person_location).to eq(board.to_s.index("@"))
     end
   end
 end
